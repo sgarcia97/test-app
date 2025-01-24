bf8922acd9f10fa60d937c4f10749fb121bf9805
sgarcia97 <83793406+sgarcia97@users.noreply.github.com>
2025-01-24 14:42:57 -0700

    Updated title on contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,17 +1,42 @@
-import {Title, Paragraph} from "../components/Title"
-import PageLayout from "../components/PageLayout"
+import React from "react"
 import {Header} from "../components/Header"
+import {Card, CardWrapper} from "../components/Card"
+import {Title} from "../components/Title"
+
 const Page = () => {
     return(
         <>
         <Header/>
         <Title title="Contact"/>
-        <PageLayout>
-            <p>Contact us.</p>
-        </PageLayout>
+        <CardWrapper center={true}>
+                <Card 
+                title="Men's Ministry" 
+                desc="Let us help you get started quickly with our helpful resources" 
+                visible={false}
+                />
+                <Card 
+                title="Women's Ministry" 
+                desc="Learn more about the gospel and what it means to be saved" 
+                visible={false}
+                />
+                <Card 
+                title="Visitation Ministry" 
+                desc="We are located at the corner of Dass and Nimblette Street, Enterprise, Chaguanas" 
+                visible={false}
+                />
+                <Card 
+                title="Media Ministry" 
+                desc="Start watching our services which are streamed live every Sunday and Tuesday" 
+                visible={false}
+                />
+                <Card 
+                title="Food Hamper Ministry" 
+                desc="Every month, the church selects a family from amongst its memebers or the community to gift a hamper to" 
+                visible={false}
+                />
+              </CardWrapper>
         </>
     )
 }
 
 export default Page;
-
