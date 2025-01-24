777ee6d77102285506683125d5ec727a75a65d8e
sgarcia97 <83793406+sgarcia97@users.noreply.github.com>
2025-01-24 00:55:40 -0700

    Fixed contact page

---
+++ app/contact/page.jsx
@@ -0,0 +1,15 @@
+import {Title, Paragraph} from "../components/Title"
+import PageLayout from "../components/PageLayout"
+import {Header} from "../components/Header"
+const Contact = () => {
+    return(
+        <>
+        <Header/>
+        <Title title="Contact"/>
+        <PageLayout></PageLayout>
+        </>
+    )
+}
+
+export default Contact;
+
