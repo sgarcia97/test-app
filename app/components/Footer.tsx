0537919755a17fa51e0c2fcf3a1b29d5173b73a3
Stefan Garcia <83793406+sgarcia97@users.noreply.github.com>
2025-01-27 16:17:33 -0700

    Update Footer.tsx

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,15 +1,19 @@
-
+import {useState} from "react"
 type Footerprop = {
 
 }
 
 const Footer = (props: Footerprop) => {
+    const [year, setYear] = useState(0)
     let d = new Date()
     let y = d.getFullYear();
+    setYear(y)
     return(
+<>
         <div className="footer">
-            <div>Copyright &copy; {y} Enterprise Bible Baptist Church. All rights reserved.
+            <div>Copyright &copy; {year} Enterprise Bible Baptist Church. All rights reserved.
         </div>
+</>
     )
 }
 
