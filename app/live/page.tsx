8083197f3dcbbf725869ff3bbead5f988bbc0c93
Stefan Garcia <83793406+sgarcia97@users.noreply.github.com>
2025-01-27 16:03:33 -0700

    Update page.tsx

--- app/live/page.tsx
+++ app/live/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import Template from "../components/Template"
+inport Countdown from "../components/Countdown
 
 export default function Page() {
   return (
     
    <Template title="Live">
     <p></p>
+    <Countdown/>
     </Template>
   );
 }
