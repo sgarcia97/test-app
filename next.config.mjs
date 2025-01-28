db5cfbbcf639ec05490747fd7efa26bb8527a25a
sgarcia97 <83793406+sgarcia97@users.noreply.github.com>
2025-01-27 17:16:50 -0700

    updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    //distDir: "./dist", // Changes the build output directory to `./dist/`.
-    webpack(config) {
-      config.module.rules.push({
-        test: /\.svg$/i,
-        issuer: /\.[jt]sx?$/,
-        use: ["@svgr/webpack"],
-      });
-      return config;
-    },
 };
 
 export default nextConfig;
