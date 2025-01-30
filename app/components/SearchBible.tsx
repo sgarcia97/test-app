b8363e8def6792e09f914c377bc65575fe903be8
sgarcia97 <83793406+sgarcia97@users.noreply.github.com>
2025-01-30 00:14:07 -0700

    updated search

--- app/components/SearchBible.tsx
+++ app/components/SearchBible.tsx
@@ -3,38 +3,48 @@ import { TfiClose } from "react-icons/tfi";
 
 const SearchBible = () => {
 const [searchData, setSearchData] = useState(null)
+const [fdata, setFData] = useState(null)
 const [data, setData] = useState(null)
 const [modal, setModal] = useState(false)
 
 const handleModal = () => {
     setModal(false)
 }
-    const search = (formData)=>{
-         const query = formData.get("query");
-         setSearchData(query)
+
+const handleResults = (e) => {
+    !e.target.value && setModal(false)
+}
+    const search = (event)=>{
+        event.preventDefault()
+        const fd = new FormData(event.target)
+        setFData(fd.get("query"))
     }
 
     useEffect(()=>{
         const queryBible = async () => {
-            const url = `https://bible-api.com/${searchData}?translation=kjv`
+            const url = `https://bible-api.com/${fdata}?translation=kjv`
             const request = await fetch(url)
+            if(request.ok){
             const data = await request.json()
             setData(data)
             setModal(true)
+            }else{
+                setModal(false)
+            }
         }
-        searchData && queryBible()
+        fdata && queryBible()
 
-    },[searchData])
+    },[fdata])
    
     return(
         <>
-        <form action={search}>
-            <input className="search" name="query" type="search" placeholder="Search the Bible"/>
+        <form onSubmit={search}>
+            <input className="search" name="query" type="search" onInput={handleResults} placeholder="Search the Bible"/>
             <button type="submit">Search</button>
         </form>
         { modal && (
         <div className="modal-bible"><div className="search-results-header"><TfiClose onClick={handleModal}/></div>{ data ? (data.verses.map((verse)=>{
-            return <div className="verse" key={verse.bible_id}><span>{verse.verse}</span>{verse.text}</div>
+            return <div className="verse" key={verse.verse}><span>{verse.verse}</span>{verse.text}</div>
         })) : "Verses"}</div>)}
         </>
     )
