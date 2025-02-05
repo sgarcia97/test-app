db10174a6fd7069eabb58ad13fcc7c12904515dd
sgarcia97 <83793406+sgarcia97@users.noreply.github.com>
2025-02-05 10:20:36 -0700

    update

--- app/churches/page.tsx
+++ app/churches/page.tsx
@@ -9,11 +9,11 @@ const Page = () => {
             <div className="church-wrapper">
         {
            churches.map(c => {
-            let url = c.url && <span> | <Link className="def-link" target="_blank" href={c.url}>Visit website</Link></span>
+            let url = c.url && <span> &#8226; <Link className="def-link" target="_blank" href={c.url}>Visit website</Link></span>
             return(
             <div key={c.name} className="church">
                 <div className="church-name">{c.name}</div>
-                <div className="church-location">{c.country} &#9679; {c.location}</div>
+                <div className="church-location">{c.country} &#8226; {c.location}</div>
                 <div className="church-location"><Link className="def-link" target="_blank" href={c.location_url}>Get directions</Link>{url}</div>
                 <div>{c.pastor}</div>
             </div>)
