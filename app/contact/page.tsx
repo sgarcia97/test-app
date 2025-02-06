1ffdfdcb69eb97f0147c9e28cdbe23d6df5e8a09
sgarcia97 <83793406+sgarcia97@users.noreply.github.com>
2025-02-05 17:24:20 -0700

    updates

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -10,7 +10,7 @@ const Page = () => {
     return(
       
         <Template title="Contact">
-<p className="page-subtitle">Welcome to Enterprise Bible Baptist! We are delighted that you’ve taken the time to reach out to us. Whether you have questions, prayer requests, or simply want to learn more about our church community, we are here to assist you. Please use the contact form below or reach out to us through any of the methods listed.</p>
+<p className="page-subtitle">Welcome to Enterprise Bible Baptist Church! We are delighted that you’ve taken the time to reach out to us. Whether you have questions, prayer requests, or simply want to learn more about our church community, we are here to assist you. Please reach out to us through any of the methods listed.</p>
         <CardWrapper center={true}>
                 <Card 
                 title="Call Us" 
