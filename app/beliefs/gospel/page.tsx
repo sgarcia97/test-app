e5e57a8236090abd2bae6715c91f09effc84689e
Stefan Garcia <83793406+sgarcia97@users.noreply.github.com>
2025-02-07 09:09:16 -0700

    Update page.tsx

--- app/beliefs/gospel/page.tsx
+++ app/beliefs/gospel/page.tsx
@@ -10,7 +10,11 @@ const Page = () => {
 
             <p>The apostle Paul gives us a definition in 1 Corinthians 15:1-4. “Moreover, brethren, I declare unto you the gospel which I preached unto you, which also ye have received, and wherein ye stand; By which also ye are saved, if ye keep in memory what I preached unto you, unless ye have believed in vain. For I delivered unto you first of all that which I also received, how that Christ died for our sins according to the scriptures; And that he was buried, and that he rose again the third day according to the scriptures:”</p>
 
-            <p>So then, the gospel is a message in three parts- 1. Christ died for our sins. 2. He was buried. 3 He rose again. Paul precedes the gospel message by speaking of its saving power and then supports his statements with the facts that Jesus was seen of Cephas (Peter), the disciples, and then five hundred others. He finishes by saying that he himself saw Jesus in a vision on a road to Damascus, declaring the saving, transformative power of believing the words of the gospel. He was a persecutor of Christians, tearing up their churches and hunting them down. When he met God and believed the gospel, his entire life changed. He became a follower of Christ.</p>
+            <p>So then, the gospel is a message in three parts:
+<ol><li>Christ died for our sins.</li>
+<li>He was buried.</li>
+<li>He rose again.</li></ol>
+Paul precedes the gospel message by speaking of its saving power and then supports his statements with the facts that Jesus was seen of Cephas (Peter), the disciples, and then five hundred others. He finishes by saying that he himself saw Jesus in a vision on a road to Damascus, declaring the saving, transformative power of believing the words of the gospel. He was a persecutor of Christians, tearing up their churches and hunting them down. When he met God and believed the gospel, his entire life changed. He became a follower of Christ.</p>
 
             <h2>The Gospel Saves</h2>
             <p>It is important to understand what the gospel does once received into the heart of a person. Paul writes in Romans 1:16 “For I am not ashamed of the gospel of Christ: for it is the power of God unto salvation to every one that believeth; to the Jew first, and also to the Greek.”</p>
